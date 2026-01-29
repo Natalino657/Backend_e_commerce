3ac9f81a4cd598cbd643444741f9359f94f2d515
Natalino Gomes <intgomes657@gmail.com>
2026-01-29 20:03:01 +0000

    orderController implemented

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,6 +3,8 @@ import Order from "../models/orderModal";
 import Product from "../models/productModel";
 import { calcPrice } from "../utils/calcPrice";
 
+//private user access
+
 const addOrderItem = asyncHandler(async (req, res) => {
   const { orderItems, shippingAddress, paymantMethod } = req.body;
 
@@ -15,4 +17,91 @@ const addOrderItem = asyncHandler(async (req, res) => {
   const itemsFromDB = await Product.find({
     _id: { $in: orderItems.map((x) => x._id) },
   });
+
+  const dbOrderItems = orderItems.map((item) => {
+    const dbItem = itemsFromDB.find((p) => p._id.toString() === item._id);
+    return {
+      ...item,
+      product: item._id,
+      price: item.price,
+      _id: undefined,
+    };
+  });
+
+  const createdOrder = await Order.save();
+  res.status(201).json(createdOrder);
 });
+
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
+});
+
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email",
+  );
+
+  if (!order) {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+  res.status(200).json(order);
+});
+
+//admin access only
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    res.status(404);
+    throw new Error("Order not Found");
+  }
+
+  order.isPaid = true;
+  order.paidAt = Date.now();
+  order.paymantResult = {
+    id: req.body.paymantId || req.body.id,
+    status: "COMPLETED",
+    update_time: req.body.update_time || new Date().toISOString(),
+    email_address: req.body.email_address || "not.provided@example.com",
+  };
+
+  //funcao para retirar item do stock quando for comprado
+  for (const item of order.orderItems) {
+    const product = await Product.findById(item.product);
+    if (!product) throw new Error(`Product ${item.product} not found`);
+    if (product.countInStock < item.qty)
+      throw new Error(`Insufficient stock for${product.name}`);
+    product.countInStock -= item.qty;
+    await product.save();
+  }
+});
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+
+  order.isDelivered = true;
+  order.deliveredAt = Date.now();
+  const updatedOrder = await order.save();
+  res.status(200).json(updatedOrder);
+});
+
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
+
+export {
+  addOrderItem,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+};
